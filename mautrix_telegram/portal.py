9803f101726cc060d14b804838ac2022577f8fc7
Akoibot <akoibot8@gmail.com>
2025-12-16 17:34:20 -0700

    Update copyright year and authors in portal.py

--- mautrix_telegram/portal.py
+++ mautrix_telegram/portal.py
@@ -1,7 +1,6 @@
 # mautrix-telegram - A Matrix-Telegram puppeting bridge
-# Copyright (C) 2023 Tulir Asokan
+# Copyright (C) 2024 Tulir Asokan & Akoi Deng
 #
-# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
